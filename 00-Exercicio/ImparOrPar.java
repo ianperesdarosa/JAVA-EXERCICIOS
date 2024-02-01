44aa45e2494d2cb4e86b37b9751bb1cd8e2fabd8
Ian <ianperesdarosa2004@gmail.com>
2024-02-01 10:48:44 -0300

    commit

--- 00-Exercicio/ImparOrPar.java
+++ 00-Exercicio/ImparOrPar.java
@@ -14,4 +14,3 @@ public class ImparOrPar {
         }
         input.close();
     }
-}
--- 00-Exercicio/primoOrNon.java
+++ 00-Exercicio/primoOrNon.java
@@ -1,25 +1,24 @@
 public class primoOrNon {
-    // Função para verificar se um número é primo
+    
     public static boolean ehPrimo(int numero) {
-        // Caso especial para números menores que 2
+        
         if (numero < 2) {
             return false;
         }
 
-        // Verifica divisibilidade até a raiz quadrada do número
+        
         for (int i = 2; i <= Math.sqrt(numero); i++) {
             if (numero % i == 0) {
-                // Se for divisível por algum número, não é primo
+                
                 return false;
             }
         }
 
-        // Se não foi divisível por nenhum número, é primo
         return true;
     }
 
     public static void main(String[] args) {
-        // Exemplo de uso
+        
         int numero = 1;
 
         if (ehPrimo(numero)) {
